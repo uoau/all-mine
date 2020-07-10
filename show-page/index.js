9c15bfb8c94dbe82aabd3c913dc7f78af328874f
luxun <401871194@qq.com>
2020-07-10 14:14:16 +0800

    Revert ":construction:  日常更新"
    This reverts commit 66426225910842f8d401a43db99db98135838cc7.

--- show-page/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2020-07-07 22:16:01
- * @Description: In User Settings Edit
- */
-
-import index from './docs/index.md';
-
-console.log(index.render);
--- show-page/nav.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2020-07-07 22:18:54
- * @LastEditTime: 2020-07-07 22:18:54
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /all-mine/show-page/nav.js
- */
