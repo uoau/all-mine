7b2bb66486360651d67056b51586c8bbf4bbefe3
luxun <401871194@qq.com>
2020-07-10 14:11:06 +0800

    Revert ":construction:  日常更新"
    This reverts commit 66426225910842f8d401a43db99db98135838cc7.

--- packages/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2020-07-07 22:04:37
- * @LastEditTime: 2020-07-07 22:04:37
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /all-mine/packages/index.js
- */ 
--- show-page/app.vue
+++
@@ -1,5 +0,0 @@
-<!--
- * @Author: your name
- * @Date: 2020-07-07 22:19:30
- * @Description: In User Settings Edit
---> 
